756abf73482a3dcfe4a073c28d914b6dea8bf9e5
Sam Deane <sam@elegantchaos.com>
2018-08-20 12:53:45 +0100

    added more targets

---
+++ Bookish/Sources/BookishMobile/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Bookish iOS
+//
+//  Created by Sam Deane on 20/08/2018.
+//  Copyright © 2018 Elegant Chaos Limited. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ Bookish/Sources/BookishModelMobile/BookishModel.swift
@@ -0,0 +1,9 @@
+//
+//  BookishModel.swift
+//  Bookish
+//
+//  Created by Sam Deane on 20/08/2018.
+//  Copyright © 2018 Elegant Chaos Limited. All rights reserved.
+//
+
+import Foundation
