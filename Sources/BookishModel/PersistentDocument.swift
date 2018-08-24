275a1efcbecf57bbf9e3f2fca1c43c4556326f38
Sam Deane <sam@elegantchaos.com>
2018-08-24 15:39:14 +0100

    Reorganised project folder. Added Dependencies/ Added Logger as a submodule.

--- Sources/BookishModel/PersistentDocument.swift
+++ Sources/BookishModel/PersistentDocument.swift
@@ -7,12 +7,16 @@
 //
 
 import AppKit
+import Logger
+
 
 open class PersistentDocument: NSPersistentDocument {
+    let modelChannel = Logger("Model")
     override open var managedObjectModel: NSManagedObjectModel {
         get {
             if let url = Bundle(for: PersistentDocument.self).url(forResource: "Document", withExtension: "momd") {
                 if let model = NSManagedObjectModel(contentsOf: url) {
+                    modelChannel.debug("Loaded model")
                     return model
                 }
             }
