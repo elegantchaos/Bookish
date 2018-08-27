977ff9dbe02290ab4ba355d2940c07dd5262c358
Sam Deane <sam@elegantchaos.com>
2018-08-27 15:50:17 +0100

    Bind detail view table programmatically.

--- Sources/BookishMac/CollectionDetailViewController.swift
+++ Sources/BookishMac/CollectionDetailViewController.swift
@@ -6,9 +6,11 @@
 import Cocoa
 import BookishModel
 
-class CollectionDetailViewController: CollectionViewController {
+class CollectionDetailViewController: CollectionViewController, NSTableViewDataSource, NSTableViewDelegate {
     @IBOutlet weak var indexView: CollectionIndexViewController!
     @IBOutlet weak var indexArray: NSArrayController!
+    
+    let headings = ["Name", "Summary"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,5 +30,32 @@ class CollectionDetailViewController: CollectionViewController {
 
         super.viewWillAppear()
     }
+ 
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return headings.count
+    }
     
+    @objc func blah() -> String {
+        return "blah"
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if row < headings.count {
+            let heading = headings[row]
+            let column = tableColumn?.identifier.rawValue
+            if column == "heading" {
+                let view = NSTextField(frame: NSZeroRect)
+                view.stringValue = heading
+                return view
+            }
+            
+            else if column == "value" {
+                let view = NSTextField(frame: NSZeroRect)
+                view.bind(NSBindingName(rawValue: "value"), to:indexArray, withKeyPath:"selection.\(heading.lowercased())", options: [:])
+                return view
+            }
+        }
+        
+        return nil
+    }
 }
