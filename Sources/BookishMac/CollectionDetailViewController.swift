0b9c87dabf66551af4bc1aac9fd0b32888da568a
Sam Deane <sam@elegantchaos.com>
2018-09-10 14:18:29 +0100

    Fixed locking of non-editable fields.

--- Sources/BookishMac/CollectionDetailViewController.swift
+++ Sources/BookishMac/CollectionDetailViewController.swift
@@ -11,18 +11,19 @@ import Dispatch
 enum RowType {
     case text
     case date
-    case dateReadOnly
 }
 
 struct RowSpecification {
     let binding: String
     let label: String
     let type: RowType
+    let editable: Bool
     
-    init(binding: String, label: String? = nil, type: RowType = .text) {
+    init(binding: String, label: String? = nil, type: RowType = .text, editable: Bool = true) {
         self.binding = binding
         self.label = label ?? binding
         self.type = type
+        self.editable = editable
     }
 }
 
@@ -43,8 +44,8 @@ class CollectionDetailViewController: CollectionViewController {
         RowSpecification(binding: "isbn"),
         RowSpecification(binding: "notes"),
         RowSpecification(binding: "published", type: .date),
-        RowSpecification(binding: "added", type: .dateReadOnly),
-        RowSpecification(binding: "modified", type: .date)
+        RowSpecification(binding: "added", type: .date, editable: false),
+        RowSpecification(binding: "modified", type: .date, editable: false)
     ]
     
     override func viewDidLoad() {
@@ -147,6 +148,16 @@ extension CollectionDetailViewController: ActionContextProvider, PersonChangeObs
 
 // MARK: Table Support
 
+protocol BindableCellView {
+    func viewToBind() -> NSView?
+}
+
+extension NSTableCellView: BindableCellView {
+    func viewToBind() -> NSView? {
+        return textField
+    }
+}
+
 extension CollectionDetailViewController: NSTableViewDataSource, NSTableViewDelegate {
     
     static let HeadingColumnID = NSUserInterfaceItemIdentifier(rawValue: "heading")
@@ -162,9 +173,10 @@ extension CollectionDetailViewController: NSTableViewDataSource, NSTableViewDele
         if isValue {
             if isPerson {
                 viewID = CollectionDetailViewController.PersonColumnID
-            } else if rows[index].type == .date {
-                viewID = CollectionDetailViewController.DateColumnID
             }
+//            else if rows[index].type == .date {
+//                viewID = CollectionDetailViewController.DateColumnID
+//            }
         }
         
         let view = tableView.makeView(withIdentifier: viewID, owner: self)
@@ -188,10 +200,16 @@ extension CollectionDetailViewController: NSTableViewDataSource, NSTableViewDele
             } else {
                 field.stringValue = rows[index].label
             }
-        } else if isValue, let subview = view?.subviews.first {
+        } else if isValue, let subview = (view as? BindableCellView)?.viewToBind() {
             var options = [NSBindingOption:Any]()
-            if !isPerson && (rows[index].type == .dateReadOnly) {
-                options[NSBindingOption(rawValue: "NSValueTransformer")] = ValueTransformer(forName: NSValueTransformerName(rawValue: "DateToString"))
+            if !isPerson && (rows[index].type == .date) {
+                options[.valueTransformer] = ValueTransformer(forName: NSValueTransformerName(rawValue: "DateToString"))
+                if let textView = subview as? NSTextField {
+                    let unlocked = rows[index].editable
+                    options[.conditionallySetsEditable] = unlocked
+                    textView.isSelectable = unlocked
+                    textView.isEditable = unlocked
+                }
             }
             let bound: Any = isPerson ? people[index] : indexView.indexArray
             let path = isPerson ? "person.name" : "selection.\(rows[index].binding)"
@@ -220,7 +238,7 @@ extension CollectionDetailViewController: NSTableViewDataSource, NSTableViewDele
             timer.invalidate()
         }
         
-        keyViewTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { _ in
+        keyViewTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { _ in
             self.recalculateKeyViews()
         }
     }
@@ -229,7 +247,7 @@ extension CollectionDetailViewController: NSTableViewDataSource, NSTableViewDele
         let rows = availableRows.sorted()
         var view: NSView = subtitleView
         for row in rows {
-            if let rowView = detailsView.view(atColumn: 1, row: row, makeIfNecessary: false)?.subviews.first {
+            if let rowView = (detailsView.view(atColumn: 1, row: row, makeIfNecessary: false) as? BindableCellView)?.viewToBind() {
                 view.nextKeyView = rowView
                 view = rowView
             }
