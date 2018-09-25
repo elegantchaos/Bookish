662a109f283714a36bc01a63a6aa5955f5aed45f
Sam Deane <sam@elegantchaos.com>
2018-09-25 17:54:37 +0100

    Adding some person detail

--- Sources/BookishMac/PersonDetailViewController.swift
+++ Sources/BookishMac/PersonDetailViewController.swift
@@ -9,5 +9,48 @@ import BookishModel
 import Dispatch
 
 class PersonDetailViewController: CollectionViewController {
+    @IBOutlet weak var nameView: NSTextField!
+    @IBOutlet weak var indexView: PersonIndexViewController!
+    var indexObserver: NSKeyValueObservation?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        indexView = nearestSibling()
+    }
     
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        
+//        if let window = view.window?.windowController as? CollectionWindowController {
+//            window.bookDetailController = self
+//        }
+        
+        if let indexArray = indexView.indexArray {
+            nameView.bind(NSBindingName(rawValue: "value"), to:indexArray, withKeyPath:"selection.name", options: [:])
+//            subtitleView.bind(NSBindingName(rawValue: "value"), to:indexArray, withKeyPath:"selection.subtitle", options: [:])
+//            imageView.bind(NSBindingName(rawValue: "value"), to:indexArray, withKeyPath:"selection.image", options: [NSBindingOption.valueTransformerName:"CoverImage"])
+            indexObserver = indexArray.observe(\NSArrayController.selection, changeHandler: { (index, change) in
+                self.selectionChanged()
+            })
+            selectionChanged()
+        }
+    }
+    
+    override func viewWillDisappear() {
+        indexObserver = nil
+        super.viewWillDisappear()
+    }
+
+    func selectionChanged() {
+//        let selectedCount = indexView.indexArray.selectedObjects?.count ?? 0
+//        let showDetail = selectedCount > 0
+//        detailsView.isHidden = !showDetail
+//        if showDetail {
+//            updatePeople()
+//        }
+        if let wc = view.window?.windowController as? CollectionWindowController {
+            wc.validateButtons()
+        }
+    }
+
 }
