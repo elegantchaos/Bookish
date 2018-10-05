9d1bca1da5c9371a4a025f478e1ab4953b67447a
Sam Deane <sam@elegantchaos.com>
2018-10-05 13:14:27 +0100

    Tidied up configs

--- Update.sh
+++ Update.sh
@@ -1,8 +1,5 @@
 swift package update
 rm -rf ".build/xcode"
 
-swift package generate-xcodeproj --output ".build/xcode/mac" --xcconfig-overrides "BookishCoreMac.xcconfig"
-#mv .build/xcode/mac/BookishCore.xcodeproj .build/xcode/mac/BookishCoreMac.xcodeproj
-
-swift package generate-xcodeproj --output ".build/xcode/mobile" --xcconfig-overrides "BookishCoreMobile.xcconfig"
-#mv .build/xcode/mobile/BookishCore.xcodeproj .build/xcode/mobile/BookishCoreMobile.xcodeproj
+swift package generate-xcodeproj --output ".build/xcode/mac" --xcconfig-overrides "Configs/BookishCoreMac.xcconfig"
+swift package generate-xcodeproj --output ".build/xcode/mobile" --xcconfig-overrides "Configs/BookishCoreMobile.xcconfig"
