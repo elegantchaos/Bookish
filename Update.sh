781f9c25e29a3990ed491b409e43bddf6a1dbf44
Sam Deane <sam@elegantchaos.com>
2018-10-05 11:55:12 +0100

    updated Upate script

--- Update.sh
+++ Update.sh
@@ -1,2 +1,3 @@
 swift package update
 swift package generate-xcodeproj --output ".build/xcode/"
+swift package generate-xcodeproj --output ".build/xcode/ios" --xconfig-overrides "BookishCoreiOS.xcconfig"
