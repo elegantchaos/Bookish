718b345a4ed3073c5b53d5c791d0d801ac82cf60
Sam Deane <sam@elegantchaos.com>
2018-10-05 13:06:39 +0100

    Sorted out building core and dependencies on iOS

--- Sources/BookishMobile/AppDelegate.swift
+++ Sources/BookishMobile/AppDelegate.swift
@@ -63,14 +63,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     }
     // MARK: - Core Data stack
 
-    lazy var persistentContainer: PersistentContainer = {
+    lazy var persistentContainer: CollectionContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = PersistentContainer(name: "Collection")
+        let container = CollectionContainer(name: "Collection")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
--- Update.sh
+++ Update.sh
@@ -1,3 +1,8 @@
 swift package update
-swift package generate-xcodeproj --output ".build/xcode/"
-swift package generate-xcodeproj --output ".build/xcode/ios" --xconfig-overrides "BookishCoreiOS.xcconfig"
+rm -rf ".build/xcode"
+
+swift package generate-xcodeproj --output ".build/xcode/mac" --xcconfig-overrides "BookishCoreMac.xcconfig"
+#mv .build/xcode/mac/BookishCore.xcodeproj .build/xcode/mac/BookishCoreMac.xcodeproj
+
+swift package generate-xcodeproj --output ".build/xcode/mobile" --xcconfig-overrides "BookishCoreMobile.xcconfig"
+#mv .build/xcode/mobile/BookishCore.xcodeproj .build/xcode/mobile/BookishCoreMobile.xcodeproj
