5bec68c4ce5b8c8e6830bc48da981e2fc3ff69ae
Sam Deane <sam@elegantchaos.com>
2018-11-14 13:23:21 +0000

    another transformer

---
+++ Sources/BookishMac/Transformers/AuthorSelectionTransformer.swift
@@ -0,0 +1,38 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 06/11/2018.
+//  All code (c) 2018 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import AppKit
+import BookishModel
+
+class AuthorSelectionTransformer: ValueTransformer {
+    static let name = NSValueTransformerName(rawValue: "PersonSelection")
+
+    var originalRole: PersonRole?
+
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let role = value as? PersonRole else {
+            return ""
+        }
+        
+        self.originalRole = role
+        return role.person?.name ?? ""
+    }
+    
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        if let text = value as? String, let originalRole = originalRole, let context = originalRole.managedObjectContext {
+            let request: NSFetchRequest<Person> = Person.fetchRequest()
+            request.predicate = NSPredicate(format: "name = \"\(text)\"")
+            request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+            if let results = try? context.fetch(request) {
+                if let person = results.first {
+                    return person
+                }
+            }
+            
+        }
+        return nil
+    }
+
+}
