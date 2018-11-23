a6d96cc871b27d471c26bc21c31d22e5e1f811c6
Sam Deane <sam@elegantchaos.com>
2018-11-23 16:21:54 +0000

    Use xcrun for the update script.

--- Package.swift
+++ Package.swift
@@ -5,23 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "BookishCore",
-    products: [
-        .library(
-            name: "BookishCore",
-            targets: ["BookishCore"])
-    ],
     dependencies: [
-        .package(url: "git@github.com:elegantchaos/Logger", from: "1.0.11"),
-        .package(url: "git@github.com:elegantchaos/Actions", from: "1.1.0"),
         .package(url: "git@github.com:elegantchaos/SketchX", from: "1.0.2"),
     ],
     targets: [
-        .target(
-            name: "BookishCore",
-            dependencies: ["Logger", "Actions", "ActionsKit"]),
-        .testTarget(
-            name: "BookishCoreTests",
-            dependencies: ["BookishCore", "Actions"]),
         ],
     swiftLanguageVersions: [.v4_2]
 )
--- Update.sh
+++ Update.sh
@@ -1,8 +1,3 @@
-swift package update
-rm -rf ".build/xcode"
-
-swift package generate-xcodeproj --output ".build/xcode/mac" --xcconfig-overrides "Configs/BookishCoreMac.xcconfig"
-swift package generate-xcodeproj --output ".build/xcode/mobile" --xcconfig-overrides "Configs/BookishCoreMobile.xcconfig"
-
+xcrun swift package update
 cd Dependencies/BookishModel
 Dependencies/Update.sh
