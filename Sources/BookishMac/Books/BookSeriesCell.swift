041fa3ef2e12e7cb38d2160a6101ff98e8e249ab
Sam Deane <sam@elegantchaos.com>
2018-12-03 17:35:51 +0000

    Better series links for fun and profit.

---
+++ Sources/BookishMac/Books/BookSeriesCell.swift
@@ -0,0 +1,40 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 03/12/2018.
+//  All code (c) 2018 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import AppKit
+import BookishModel
+import Actions
+
+class BookSeriesCell: NSTableCellView {
+    @IBOutlet weak var seriesField: NSTextField!
+    @IBOutlet weak var indexField: NSTextField!
+    var detailView: BookDetailViewController?
+}
+
+extension BookSeriesCell: BookDetailTableCell {
+    func setup(for view: BookDetailViewController, row: DetailDataSource.RowInfo) {
+        assert(row.category == .series)
+        let source = view.source
+        detailView = view
+        let entry = source.series(for: row)
+        objectValue = entry
+        if let name = entry.series?.name {
+            seriesField?.stringValue = name
+        }
+        indexField.integerValue = Int(entry.index)
+    }
+    
+    func keyView() -> NSView? {
+        return seriesField
+    }
+}
+
+extension BookSeriesCell: ActionContextProvider {
+    func provide(context: ActionContext) {
+        if let entry = objectValue as? Entry, let series = entry.series {
+            context.info[SeriesAction.seriesKey] = series
+        }
+    }
+}
