08935a12a4ad6d8636ddbf50140d4bf39d76a6d0
Sam Deane <sam@elegantchaos.com>
2019-02-26 13:06:14 +0000

    Started adding scanner window.

---
+++ Sources/BookishMac/Controllers/ScannerViewController.swift
@@ -0,0 +1,72 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 26/02/2019.
+//  All code (c) 2019 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import AppKit
+import AVFoundation
+import Vision
+
+class ScannerViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    
+    @IBOutlet weak var imageView: NSView!
+    
+    var captureDevice: AVCaptureDevice!
+    var session = AVCaptureSession()
+    var requests = [VNRequest]()
+    
+    override func viewDidLoad() {
+        self.viewDidLoad()
+        self.setupVideo()
+        self.startDetection()
+    }
+    
+    func setupVideo() {
+        session.sessionPreset = AVCaptureSession.Preset.photo
+        captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        let deviceInput = try! AVCaptureDeviceInput(device: captureDevice!)
+        let deviceOutput = AVCaptureVideoDataOutput()
+        deviceOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA)]
+        deviceOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.default))
+        session.addInput(deviceInput)
+        session.addOutput(deviceOutput)
+        let imageLayer = AVCaptureVideoPreviewLayer(session: session)
+        imageLayer.frame = imageView.bounds
+        imageView.layer?.addSublayer(imageLayer)
+        session.startRunning()
+    }
+    
+    func startDetection() {
+        let request = VNDetectBarcodesRequest(completionHandler: self.detectHandler)
+        request.symbologies = [VNBarcodeSymbology.Code39] // or use .QR, etc
+        self.requests = [request]
+    }
+    
+    func detectHandler(request: VNRequest, error: Error?) {
+        guard let observations = request.results else {
+            //print("no result")
+            return
+        }
+        let results = observations.map({$0 as? VNBarcodeObservation})
+        for result in results {
+            print(result!.payloadStringValue!)
+        }
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        var requestOptions:[VNImageOption:Any] = [:]
+        if let camData = CMGetAttachment(sampleBuffer, key: kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, attachmentModeOut: nil) {
+            requestOptions = [.cameraIntrinsics:camData]
+        }
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation(rawValue: 6)!, options: requestOptions)
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
+}
