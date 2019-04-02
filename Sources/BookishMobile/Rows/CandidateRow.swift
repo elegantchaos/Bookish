8ea96da5931155b53dace661cdfd611de1a58afc
Sam Deane <sam@elegantchaos.com>
2019-04-02 10:42:54 +0100

    added image to scanner candidates

--- Sources/BookishMobile/Rows/CandidateRow.swift
+++ Sources/BookishMobile/Rows/CandidateRow.swift
@@ -9,9 +9,17 @@ import BookishModel
 class CandidateRow: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var authorsLabel: UILabel!
+    @IBOutlet weak var coverView: UIImageView!
     
     func setup(with candidate: LookupCandidate) {
         titleLabel.text = candidate.summary
         authorsLabel.text = candidate.authors.joined(separator: ", ")
+
+        if let urlString = candidate.image, let url = URL(string: urlString) {
+            application.imageCache.image(for: url) { (image) in
+                self.coverView.image = image
+            }
+        }
+
     }
 }
