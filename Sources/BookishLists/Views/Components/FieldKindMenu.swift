bed615fd831d1fd7934d6748962530e2850f8052
Sam Deane <sam@elegantchaos.com>
2021-02-17 15:59:20 +0000

    Improved menu

--- Sources/BookishLists/Views/Components/FieldKindMenu.swift
+++ Sources/BookishLists/Views/Components/FieldKindMenu.swift
@@ -7,41 +7,23 @@ import SwiftUI
 
 struct FieldKindMenu: View {
     @ObservedObject var field: ListField
-
-    var label: some View {
-        HStack {
-            Text(field.kind.label)
-            Image(systemName: "chevron.up.chevron.down")
+    
+    var body: some View {
+        Menu {
+            Picker("", selection: $field.kind) {
+                ForEach(ListField.Kind.allCases, id: \.self) { kind in
+                    Text(kind.label)
+                }
+            }
+            .pickerStyle(InlinePickerStyle())
+        } label: {
+            HStack {
+                Text(field.kind.label)
+                Image(systemName: "chevron.up.chevron.down")
+            }
         }
     }
     
-    var body: some View {
-
-        Menu {
-        Picker("", selection: $field.kind) {
-            ForEach(ListField.Kind.allCases, id: \.self) { kind in
-                Text(kind.label)
-            }
-        }
-        .pickerStyle(InlinePickerStyle())
-        } label: {
-            label
-        }
-        
-//        Menu {
-//            ForEach(ListField.Kind.allCases, id: \.self) { kind in
-//                Button(action: { handleSetKind(to: kind) }) {
-//                    Text(kind.label)
-//                }
-//            }
-//        } label: {
-//            HStack {
-//                Text(field.kind.label)
-//                Image(systemName: "arrowtriangle.down")
-//            }
-//        }
-    }
-
     func handleSetKind(to kind: ListField.Kind) {
         field.kind = kind
     }
