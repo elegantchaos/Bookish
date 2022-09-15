08e46049a2b6737c07f145843e2775e7f39e2c29
Sam Deane <sam@elegantchaos.com>
2022-09-15 17:49:30 +0100

    Renamed content view, removed Application

--- Dependencies/BookishViews/Sources/BookishViews/Views/RootView.swift
+++ Dependencies/BookishViews/Sources/BookishViews/Views/RootView.swift
@@ -9,42 +9,7 @@ import BookishImporter
 import UniformTypeIdentifiers
 import CoreData
 
-public class FilePickerController: ObservableObject {
-    @Published var importRequested = false
-    @Published var exportRequested = false
-    @Published var exportDocument: BookishInterchangeDocument?
-    @Published var importContentTypes: [UTType] = []
-    @Published var exportContentType: UTType = .json
-    @Published private var importCompletion: ((_ result: Result<URL,Error>) -> ())?
-    @Published private var exportCompletion: ((_ result: Result<URL,Error>) -> ())?
-
-    public init() {
-    }
-
-    func chooseFileToImport(completion: @escaping (_ result: Result<URL,Error>) -> ()) {
-        importCompletion = completion
-        importRequested = true
-    }
-
-    func chooseLocationToExport(_ document: BookishInterchangeDocument, completion: @escaping (_ result: Result<URL,Error>) -> ()) {
-        exportDocument = document
-        exportCompletion = completion
-        exportRequested = true
-    }
-
-    func handlePerformImport(_ result: Result<URL,Error>) {
-        importCompletion?(result)
-        importCompletion = nil
-    }
-
-    func handlePerformExport(_ result: Result<URL,Error>) {
-        exportCompletion?(result)
-        exportCompletion = nil
-    }
-
-}
-
-public struct ContentView: View {
+public struct RootView: View {
     @EnvironmentObject var fileController: FilePickerController
     @EnvironmentObject var importController: ImportController
     @EnvironmentObject var statusController: StatusController
